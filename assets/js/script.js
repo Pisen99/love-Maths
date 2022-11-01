c4c10847032638bb5890ad10ec886bada72f995c
Pisen99 <patricia.andersson99@hotmail.com>
2022-11-01 16:57:22 +0000

    Display addition questions to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,10 +10,12 @@ document.addEventListener("DOMContentLoaded", function() {
                 alert("You clicked the Submit!")
             } else {
                 let gameType = this.getAttribute("data-type")
-                alert(`You clicked ${gameType}`);
+                runGame(gameType);
             }
         })
     }
+
+    runGame("addition");
 })
 
 
@@ -21,10 +23,17 @@ document.addEventListener("DOMContentLoaded", function() {
  * The main game "loop", called wheb the script is first loaded
  * and after the users answer has been processed
  */
-function runGame(){
+function runGame(gameType){
     // Creates two random numbers between 1 and 25
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
+
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert(`Unknown game type: ${gameType}`)
+        throw `Unknown game type: ${gameType}. Aborting!`;
+    }
 }
 
 
@@ -44,8 +53,10 @@ function incrementWrongAnswer(){
     
 }
 
-function displayAdditionQuestion(){
-    
+function displayAdditionQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "+";
 }
 
 function displaySubtractQuestion(){
